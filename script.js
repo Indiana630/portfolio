afc66a5ed1e380f57404b7e98a3192cf0e9f97e5
Indiana630 <125204578+Indiana630@users.noreply.github.com>
2024-02-17 16:16:41 +0100

    rt

--- script.js
+++ script.js
@@ -30,12 +30,18 @@ window.onscroll = function() { efectoHabilidades() };
 //funcion que aplica la animación de la barra de habilidades
 function efectoHabilidades() {
     var skills = document.getElementById("skills");
+    var container = document.querySelector('#sobremi');
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
         document.getElementById("html").classList.add("barra-progreso1");
         document.getElementById("js").classList.add("barra-progreso2");
         document.getElementById("bd").classList.add("barra-progreso3");
         document.getElementById("ps").classList.add("barra-progreso4");
-    }   
+    }
+    
+    if (distancia_skills >= 20) {
+        container.classList.add('show'); 
+    }
+
 
 }
